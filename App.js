bc21c1e120e2f77d378fef9e6bc049ca36f5843e
Jinit-Jain <jinit2000@yahoo.com>
2019-12-30 16:58:59 +0530

    Update App.js
    Added bottom-tab-navigator and icons.

--- App.js
+++ App.js
@@ -1,22 +1,80 @@
-import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
-import { createAppContainer} from 'react-navigation';
-import Artists from './screens/Artists'
+import {createAppContainer} from 'react-navigation';
+import Artists from './screens/Artists';
+import Albums from './screens/Albums';
+import TagTopArtist from './screens/TagTopArstist';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  StatusBar,
+  TouchableOpacity,
+  Alert,
+  Linking,
+} from 'react-native';
 
+import Tracks from './screens/Tracks';
+const TabNavigator = createMaterialBottomTabNavigator({
+  Tracks: {
+    screen: Tracks,
+    navigationOptions: {
+      tabBarIcon: () => (
+        <View>
+          <Icon name={'music'} size={20} color="white"></Icon>
+        </View>
+      ),
+      tabBarLabel: <Text style={{color:"yellow"}}>Track</Text>
+    },
+  },
+  Albums: {
+    screen: Albums,
+    navigationOptions: {
+      tabBarOptions: { activeTintColor:'red'},
+      tabBarIcon: () => (
+        <View>
+          <Icon name={'book'} size={20} color="white"></Icon>
+        </View>
+      ),
+      tabBarLabel: <Text style={{color:"yellow"}}>Album</Text>
+    },
+    
+  },
 
-const navigator=createStackNavigator({
-    Search: Artists
+  Artists: {
+    screen: Artists,
+    navigationOptions: {
+      tabBarIcon: () => (
+        <View>
+          <Icon name={'group'} size={20} color="white"></Icon>
+        </View>
+      ),
+      tabBarLabel:  <Text style={{color:"yellow"}}>Artists</Text>   },
+  },
+
+  TagTopArtist: {
+    screen: TagTopArtist,
+    navigationOptions: {
+      tabBarIcon: () => (
+        <View>
+          <Icon name={'flash'} size={20} color="white"></Icon>
+        </View>
+      ),
+      tabBarLabel:  <Text style={{color:"yellow"}}>Trending</Text>
+    },
+  },
 },
 {
-    initialRouteName:'Search',
-    defaultNavigationOptions:{
-        title : 'Albums'
-    }
+  activeColor: '#f0edf6',
+    inactiveColor: '#3e2465',
+    
+  barStyle: { backgroundColor: '#193366' },
 });
 
-const Appcontainer=createAppContainer(navigator);
-
-export default App=()=>{
-    return <Appcontainer/>;
-}
+const App = createAppContainer(TabNavigator);
+export default App;
